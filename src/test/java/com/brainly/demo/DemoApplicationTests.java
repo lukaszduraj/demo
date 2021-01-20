e2a56cd249ad5f759395efbedf64ebbf497e6dbe
Lukasz Duraj <lukasz.duraj@brainly.com>
2021-01-20 08:44:33 +0100

    spring boot - initial commit

---
+++ src/test/java/com/brainly/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainly.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
