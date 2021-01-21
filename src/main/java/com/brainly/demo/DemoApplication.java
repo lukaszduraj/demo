3b3a7adbe40af72efa93ab898564c145508151e0
Lukasz Duraj <lukasz.duraj@brainly.com>
2021-01-21 10:00:44 +0100

    Add Chinock JPA enitities

--- src/main/java/com/brainly/demo/DemoApplication.java
+++ src/main/java/com/brainly/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.brainly.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.brainly.demo"})
+@EnableJpaRepositories(basePackages = {"com.chinook.model"})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
